8ee9abb9737fb721a2b7ccac12e69a12fe9ab1f6
nerongnyr <163106852+nerongnyr@users.noreply.github.com>
2024-06-12 12:16:12 +0900

    Add files via upload

--- MainPage.jsx
+++ MainPage.jsx
@@ -1,19 +1,64 @@
-import { useState } from "react"
-import WarningBanner from "./WarningBanner"
+import styled from "styled-components"
 
-export default function MainPage(props) {
-    const [showWarning, setShowWarning] = useState(false)
+const Wrapper = styled.div`
+    padding: lem;
+    background: gray
+`
 
-    const handleToggleClick = () => {
-        setShowWarning(prevShowWarning => !prevShowWarning)
+const Title = styled.h1`
+    font-size: 1.5em;
+    color: white;
+    text-align: center
+`
+// const Button = styled.button`
+//     color: ${props => props.dark ? 'white' : 'dark'};
+//     background: ${props => props.dark ? 'black' : 'white'};
+//     border: 1px solid black;
+// `
+// const RoundButton = styled(Button)`
+//     border-radius: 16px;
+// `
+
+const Block = styled.div`
+    border: 1px solid black
+`
+
+const blockItem = [
+    {
+        lavel: '1',
+        padding: '1rem',
+        backgroundColor: 'red'
+    },
+    {
+        lavel: '2',
+        padding: '3rem',
+        backgroundColor: 'green'
+    },
+    {
+        lavel: '3',
+        padding: '4rem',
+        backgroundColor: 'blue'
     }
+]
 
-    return(
-        <div>
-            <WarningBanner warning = {showWarning} />
-            <button onClick={handleToggleClick}>
-                {showWarning ? '감추기' : '보이기'}
-            </button>
-        </div>
+export default function MainPage() {
+    return (
+        <Wrapper>
+            <Title>
+                안녕 리액트!
+            </Title>
+            {/* <Button>Normal</Button>
+            <Button dark>Dark</Button>
+            <RoundButton>Round Button</RoundButton> */}
+            <br /> <br />
+            {blockItem.map((blockItem) => {
+                <Block
+                    padding={blockItem.padding}
+                    backgroundColor={blockItem.backgroundColor}
+                >
+                    {blockItem.lavel}
+                </Block>
+            })}
+        </Wrapper>
     )
 }
